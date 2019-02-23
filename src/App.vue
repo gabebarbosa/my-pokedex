2bf2e5554a4ac0467709a66aae095711c0ae6198
gabebarbosa <gabriel.lemosbarbosa@gmail.com>
2019-02-23 07:04:14 -0300

    primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Pokemon titulo="lista de pokemons"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Pokemon from './components/Pokemon.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Pokemon
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import '../node_modules/bootstrap/dist/css/bootstrap.css';
 </style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ titulo }}</h1>
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Nome</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="pokemon in pokemons" :key="pokemon.name">
+          <td>{{pokemon.name}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Pokemon',
+  props: {
+    titulo: String
+  },
+  data () {
+    return {
+      pokemons: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1000')
+      .then(response => (this.pokemons = response.data.results))
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
